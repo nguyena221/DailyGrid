f49d91d1e08c9adde1d8cd415521b5aac40dfac5
Annie Nguyen <nguyenannie221@gmail.com>
2025-12-30 17:51:57 -0500

    Added to AuthLayout.tsx

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,11 +1,14 @@
 // Creates a frame for Login & Signup (Sets up a box/layout for authorization)
+// Use Outlet to prevent repetition if background is the same, etc., URL never changes, resets app, etc. 
+// Pages = content; Layouts = things that are shared
 import {Outlet} from "react-router-dom";
 
 function AuthLayout() {
     return (
-        <div>
-            <Outlet />
-        </div>
+      <div>
+        {/* can put the shared UI here */}
+        <Outlet />
+      </div>
     );
   }
   
