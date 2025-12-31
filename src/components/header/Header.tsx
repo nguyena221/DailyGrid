3905380cb9994c7a5b42d818fa71ee9580d0e0c1
Annie Nguyen <nguyenannie221@gmail.com>
2025-12-31 18:56:43 -0500

    Imported Logo. Included it in Header file.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import ViewTabs from "./ViewTabs";
 import ProfileButton from "./ProfileButton";
+import DGLogo from "../../assets/images/DGLogo.png";
 
 function Header() {
     <header className="header">
         <div className="header-left">
-
+            <button>
+                <img src={DGLogo} alt="Logo" />
+            </button>
         </div>
         <div className="header-center">
 
