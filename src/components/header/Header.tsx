c92c842618b1719c04c0f0091559fd8ed262f74a
Annie Nguyen <nguyenannie221@gmail.com>
2025-12-31 18:39:43 -0500

    Set up the header structure & components.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import ViewTabs from "./ViewTabs";
+import ProfileButton from "./ProfileButton";
+
+function Header() {
+    <header className="header">
+        <div className="header-left">
+
+        </div>
+        <div className="header-center">
+
+        </div>
+        <div className="header-right">
+
+        </div>
+    </header>
+}
