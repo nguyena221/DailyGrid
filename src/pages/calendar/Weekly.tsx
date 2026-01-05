b7ef3f6e26fe927438776e72f5fb34ec83e301dd
Annie Nguyen <nguyenannie221@gmail.com>
2026-01-05 01:37:25 -0500

    Imported Calendar Header to Weekly.tsx

--- src/pages/calendar/Weekly.tsx
+++ src/pages/calendar/Weekly.tsx
@@ -1,4 +1,12 @@
-export default function Weekly() {
-    return <h1>Weekly</h1>;
-  }
-  
+import { useState } from "react";
+import CalendarHeader from "../../components/calendar/CalendarHeader";
+
+function Weekly() {
+  const [date, setDate] = useState(() => new Date());
+
+  return (
+    <CalendarHeader view="weekly" date={date} onDateChange={setDate} />
+  );
+}
+
+export default Weekly;
