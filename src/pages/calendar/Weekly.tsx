421063f7fb8a20176d57258b24adf50f319ea52f
Annie Nguyen <nguyenannie221@gmail.com>
2026-01-10 01:29:16 -0500

    Fixes to Weekly.tsx; adds button

--- src/pages/calendar/Weekly.tsx
+++ src/pages/calendar/Weekly.tsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
 import CalendarHeader from "../../components/calendar/CalendarHeader";
 import WeekGrid from "../../components/calendar/WeekGrid";
+import "../styles/Weekly.css";
 
 export default function Weekly() {
   const [date, setDate] = useState<Date>(new Date());
+  const [showHours, setShowHours] = useState<boolean>(false);
 
   return (
     <div>
       <CalendarHeader view="weekly" date={date} onDateChange={setDate} />
 
+      <div className="weekly-controls">
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={showHours}
+            onChange={(e) => setShowHours(e.target.checked)}
+          />
+          <span className="slider" />
+        </label>
+        <span className="switch-label">Hourly</span>
+      </div>
+
       <WeekGrid
         date={date}
         startOnMonday={false}
+        showHours={showHours}
         startHour={9}
         endHour={20}
         onCellClick={(day, hour) => console.log("clicked", day, hour)}
