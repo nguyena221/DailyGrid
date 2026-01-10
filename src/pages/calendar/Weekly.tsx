f41c0b2e24865f14947b055a6d8fcedaef768815
Annie Nguyen <nguyenannie221@gmail.com>
2026-01-10 01:30:45 -0500

    Created and implemented Weekly.css

--- src/pages/calendar/Weekly.tsx
+++ src/pages/calendar/Weekly.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import CalendarHeader from "../../components/calendar/CalendarHeader";
 import WeekGrid from "../../components/calendar/WeekGrid";
-import "../styles/Weekly.css";
+import "../../styles/Weekly.css";
 
 export default function Weekly() {
   const [date, setDate] = useState<Date>(new Date());
