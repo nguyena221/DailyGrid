ea365c9a992fbeb7ad3f7758a346bd182a24a033
Annie Nguyen <nguyenannie221@gmail.com>
2026-01-10 01:23:24 -0500

    Created WeekGrid.css. Added WeekGrid to Weekly.tsx

--- src/pages/calendar/Weekly.tsx
+++ src/pages/calendar/Weekly.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import CalendarHeader from "../../components/calendar/CalendarHeader";
+import WeekGrid from "../../components/calendar/WeekGrid";
 
-function Weekly() {
-  const [date, setDate] = useState(() => new Date());
+export default function Weekly() {
+  const [date, setDate] = useState<Date>(new Date());
 
   return (
-    <CalendarHeader view="weekly" date={date} onDateChange={setDate} />
+    <div>
+      <CalendarHeader view="weekly" date={date} onDateChange={setDate} />
+
+      <WeekGrid
+        date={date}
+        startOnMonday={false}
+        startHour={9}
+        endHour={20}
+        onCellClick={(day, hour) => console.log("clicked", day, hour)}
+        renderCell={() => null}
+      />
+    </div>
   );
 }
-
-export default Weekly;
